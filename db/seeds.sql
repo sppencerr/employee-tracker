27843949b772b90bc3616f6481ff3875a27b326d
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-12-21 21:47:28 -0700

    seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -24,16 +24,16 @@ VALUES ("Legal Team Lead", 250000, 4);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Payne", " Collinge", 2, 1);
+VALUES ("Mike", " Chan", 2, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Orvyn", "Beckey", 3, null);
+VALUES ("Ashley", "Rodriguez", 3, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Edmond", "Hatchell", 4, 3);
+VALUES ("Kevin", "Tupik", 4, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Lin", "Barfield", 5, null);
+VALUES ("Kunal", "Singh", 5, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Lucan", "Farabee", 2, null);
+VALUES ("Malia", "Brown", 2, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Merlin", "Collins", 4, 7);
+VALUES ("Sarah", "Lourd", 4, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Orson", "Higgason", 1, 2);
+VALUES ("Tom", "Allen", 1, 2);
